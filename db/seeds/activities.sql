0d7f6075532c1bdbc12d81d56a39cb13520d0184
Chris Cho <hyunsangc@gmail.com>
2021-10-27 21:32:23 -0400

    add users and activities schema and seeds

---
+++ db/seeds/activities.sql
@@ -0,0 +1,14 @@
+INSERT INTO books (rating, author, title, genre)
+VALUES
+  (4, 'E. B. White', 'Charlottes Web', 'Children'),
+  (3.5, 'J. K. Rowling', 'Harry Potter', 'Fantasy');
+
+INSERT INTO movies (rating, director, title, genre)
+VALUES
+  (4, 'James Cameron', 'Avatar', 'Sci-fi'),
+  (5, 'Christopher Nolan', 'Inception', 'Action');
+
+INSERT INTO products (rating, name, category, price)
+VALUES
+  (3, 'USB hub', 'Electronics', 1399),
+  (4, 'LG OLED TV', 'Electronics', 169900);
