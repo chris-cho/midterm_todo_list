4679959104c7c740bd2c0be37c0fa36997ba5201
Chris Cho <hyunsangc@gmail.com>
2021-10-28 16:08:12 -0400

    add products API

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,51 +4,24 @@ const router  = express.Router();
 const requests = require('request');
 
 module.exports = (db) => {
-  router.get("/movies", (req, res) => {
+  router.get("/products", (req, res) => {
     const requestOptions = {
-      url: 'https://imdb8.p.rapidapi.com/title/get-top-rated-movies',
+      url: 'https://fakestoreapi.com/products?limit=5',
       method: 'GET',
-      headers: {
-        'x-rapidapi-key': '2c637a1079msha9d728c69ee5dfep1d7adfjsn129040aa0881',
-        'x-rapidapi-host': 'imdb8.p.rapidapi.com'
-      }
     };
     requests(requestOptions, (err, response, body) => {
       if (err) {
         console.log(err);
       } else if (response.statusCode === 200) {
-        const movies = JSON.parse(body);
-        const sliced = Object.keys(movies).slice(0, 4).reduce((result, key) => {
-                    result[key] = movies[key];
-                    return result;
-                }, {});
-        for (const key of Object.keys(sliced)) {
-            const requestOptions = {
-              url: 'https://imdb8.p.rapidapi.com/title/get-meta-data',
-              method: 'GET',
-              qs: {
-                ids: sliced[key].id.slice(7,16)
-              },
-              headers: {
-                'x-rapidapi-key': '2c637a1079msha9d728c69ee5dfep1d7adfjsn129040aa0881',
-                'x-rapidapi-host': 'imdb8.p.rapidapi.com'
-              }
-            };
-            requests(requestOptions, (err, response, body) => {
-              if (err) {
-                console.log(err);
-              } else if (response.statusCode === 200) {
-                const details = JSON.parse(body);
-                console.log(details[sliced[key].id.slice(7,16)].title.title.image.url, details[sliced[key].id.slice(7,16)].title.title, details[sliced[key].id.slice(7,16)].ratings.rating, details[sliced[key].id.slice(7,16)].genres[0], );
-              } else {
-                console.log(response.statusCode);
-              }
-            })
-          }
-        } else {
-          console.log(response.statusCode);
+        const products = JSON.parse(body);
+        console.log(products[0]);
+        for (const product of products) {
+          console.log(product.title, product.price, product.image, product.category);
         }
+      } else {
+        console.log(response.statusCode);
+      }
     });
-            });
+  });
   return router;
 };
